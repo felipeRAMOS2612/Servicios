c9e623970bb05bd86cdc08f59f21ae3007ba1561
Felipe Ramos <feliperamos2612@gmail.com>
2022-12-18 02:34:10 -0300

    arreglo php

--- correo.php
+++ correo.php
@@ -1,19 +1,33 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+$result = "";
 
+if(isset($_POST['enviar'])){
+  $result = $_POST['nombre'];
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/SMTP.php';
+    $mail = new PHPMailer;
+    $mail -> isSMTP();
+    $mail -> Host ='smtp.gmail.com';
+    $mail -> Port = 587;
+    $mail ->SMTPAuth = true;
+    $mail ->SMTPSecure = 'tls';
+    $mail ->Username = 'generalmantencion2022@gmail.com';
+    $mail ->Password = 'kyzevslkykdefkpb';
 
-$name = $_POST["nombre"];
-$email = $_POST["email"];
-$cel = $_POST["telefono"];
-$message = $_POST["mensaje"];
+    $mail -> setFrom($_POST['email'], $_POST['nombre']);
+    $mail -> addAddress('generalmantencion2022@gmail.com');
+    $mail -> addReplyTo($_POST['email'],$_POST['nombre']);
 
-$destinatario = "generalmantencion2022@gmail.com";
-$asunto = "Mensaje desde la pagina web";
-
-$carta = "Nombre: $name \n";
-$carta .= "Telefono: $cel \n";
-$carta .= "mensaje: $message \n";
-$header = "enviado desde $email";
-
-mail($destinatario, $asunto, $carta, $header);
-
+    $mail -> isHTML(true);
+    $mail -> Subject='Correo enviado desde pagina web';
+    $mail -> Body = '<h3 align=center>Correo: '.$_POST['email'].'<br>Nombre: '.$_POST['nombre'].'<br>Telefono: '.$_POST['telefono'].'<br>Mensaje: '.$_POST['mensaje'].'</h3>';
+    if(!$mail -> send()){
+      $result = "Algo ocurrio mal, intentalo denuevo mas tarde";
+    } else{
+      $result = "Correo enviado con exito";
+    }
+  }
 ?>
